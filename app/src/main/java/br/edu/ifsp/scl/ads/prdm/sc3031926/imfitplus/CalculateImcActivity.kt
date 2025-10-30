ad0b776a9327c1a936f669d7b72515a46efa0381
otaviopl <otavio.pereira.lopes@gmail.com>
2025-10-30 20:50:34 -0300

    add second screen mvp

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3031926/imfitplus/CalculateImcActivity.kt
@@ -0,0 +1,16 @@
+package br.edu.ifsp.scl.ads.prdm.sc3031926.imfitplus
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.scl.ads.prdm.sc3031926.imfitplus.databinding.CalculateImcBinding
+
+class CalculateImcActivity: AppCompatActivity() {
+    private lateinit var binding: CalculateImcBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = CalculateImcBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3031926/imfitplus/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3031926/imfitplus/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.edu.ifsp.scl.ads.prdm.sc3031926.imfitplus
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.scl.ads.prdm.sc3031926.imfitplus.databinding.MainActivityBinding
@@ -12,11 +13,13 @@ class MainActivity : AppCompatActivity() {
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupToolbar(binding)
-
+        binding.buttonStart.setOnClickListener{
+            Intent("CALCULATE_IMC").let {this.startActivity(it)}
+        }
     }
 
     private fun setupToolbar(binding: MainActivityBinding) {
         setSupportActionBar(binding.toolbar.toolbar)
-        supportActionBar?.title = "Seja bem bindo!"
+        supportActionBar?.title = "Seja bem vindo!"
     }
 }
