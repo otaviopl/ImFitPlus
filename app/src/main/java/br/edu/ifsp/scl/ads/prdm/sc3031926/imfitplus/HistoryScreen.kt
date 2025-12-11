85434f6463520a2565868042ad5e9433763360b3
otaviopl <otavio.pereira.lopes@gmail.com>
2025-12-11 19:55:38 -0300

    start history screen initial config

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3031926/imfitplus/HistoryScreen.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3031926/imfitplus/HistoryScreen.kt
@@ -1,4 +1,27 @@
 package br.edu.ifsp.scl.ads.prdm.sc3031926.imfitplus
 
-class HistoryScreen {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.scl.ads.prdm.sc3031926.imfitplus.databinding.HistoryScreenBinding
+import br.edu.ifsp.scl.ads.prdm.sc3031926.imfitplus.databinding.ResultActivityBinding
+
+class HistoryScreen : AppCompatActivity() {
+    private lateinit var binding: HistoryScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = HistoryScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupToolbar()
+        binding.btnBackHome.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
+        }    }
+    private fun setupToolbar() {
+        setSupportActionBar(binding.toolbar.toolbar)
+        supportActionBar?.title = "Histórico de Usuários"
+    }
 }
